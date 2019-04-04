7b7b5b667fb11adaf616d95ce2b6cdba9561d9a6
Letitia Kwan <letitia.kwan@gmail.com>
2019-04-04 17:08:40 -0400

    BStrategy code inside

--- CS2212B-Project-BaseCode/src/strategies/publisher/BStrategy.java
+++ CS2212B-Project-BaseCode/src/strategies/publisher/BStrategy.java
@@ -1,5 +1,18 @@
 package strategies.publisher;
 
-public class BStrategy {
+import events.AbstractEvent;
 
+public class BStrategy extends AbstractStrategy {
+	protected BStrategy (int publisherId)
+	{
+		super(publisherId);
+	}
+	
+	protected BStrategy(int publisherId, AbstractEvent event)
+	{
+		super(publisherId, event);
+	}
+	
+	
 }
+
