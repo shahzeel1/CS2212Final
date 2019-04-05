8bdefc538bb8ee6648b2181b4722d06580be4ed8
Zeel <shahzeel860@gmail.com>
2019-04-05 13:58:14 -0400

    Added parse

--- CS2212B-Project-BaseCode/src/orchestration/Orchestration.java
+++ CS2212B-Project-BaseCode/src/orchestration/Orchestration.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import pubSubServer.AbstractChannel;
 import pubSubServer.ChannelDiscovery;
@@ -26,24 +27,73 @@ public class Orchestration {
 		List<AbstractPublisher> listOfPublishers = new ArrayList<>();
 		List<AbstractSubscriber> listOfSubscribers = new ArrayList<>();
 		Orchestration testHarness = new Orchestration();
+
+		//Method to parse the driver.txt file
+		BufferedReader driver = new BufferedReader(new FileReader(new File("driver.txt")));
+		String line = driver.readLine();
+		while(line != null) {
+
+			String action;
+			int pub_id;
+			int sub_id;
+			String event_type;
+			String event_header;
+			String event_payload;
+			String channel_name;
+
+			StringTokenizer st = new StringTokenizer(line);
+			String firstWord = st.nextToken();
+
+			if (firstWord.equals("SUB")) {
+				action = "subscribe";
+				sub_id = Integer.parseInt(st.nextToken());
+				channel_name = st.nextToken();
+			}
+			else if(firstWord.equals("PUB")) {
+				action = "publish";
+				pub_id = Integer.parseInt(st.nextToken());
+				//if MORE parameters are provided
+				if(st.hasMoreTokens()) {
+					event_type = st.nextToken();
+					event_header = st.nextToken();
+					event_payload = st.nextToken();
+				}
+			}
+			else if(firstWord.equals("BLOCK")) {
+				action = "block";
+				sub_id = Integer.parseInt(st.nextToken());
+				channel_name = st.nextToken();
+			}
+			else if(firstWord.equals("UNBLOCK")) {
+				action = "unblock";
+				sub_id = Integer.parseInt(st.nextToken());
+				channel_name = st.nextToken();
+			}
+			else {
+				System.out.println("Error reading driver.txt!");
+				break;
+			}
+			line = driver.readLine();
+		}
+
 		try {
 			listOfPublishers = testHarness.createPublishers();
 			listOfSubscribers = testHarness.createSubscribers();
-			
+
 			List<AbstractChannel> channels = ChannelDiscovery.getInstance().listChannels();
 			//For demonstration purposes only
 			try {
-			BufferedReader initialChannels = new BufferedReader(new FileReader(new File("Channels.chl")));
-			List<String> channelList = new ArrayList<String>();
-			String line = "";
-			while((line = initialChannels.readLine()) != null )
-				channelList.add(line);
-			int subscriberIndex = 0;
-			for(AbstractSubscriber subscriber : listOfSubscribers) {
-				subscriber.subscribe(channelList.get(subscriberIndex%channelList.size()));
-				subscriberIndex++;
-			}
-			initialChannels.close();
+				BufferedReader initialChannels = new BufferedReader(new FileReader(new File("Channels.chl")));
+				List<String> channelList = new ArrayList<String>();
+				String line = "";
+				while((line = initialChannels.readLine()) != null )
+					channelList.add(line);
+				int subscriberIndex = 0;
+				for(AbstractSubscriber subscriber : listOfSubscribers) {
+					subscriber.subscribe(channelList.get(subscriberIndex%channelList.size()));
+					subscriberIndex++;
+				}
+				initialChannels.close();
 			}catch(IOException ioe) {
 				System.out.println("Loading Channels from file failed proceeding with random selection");
 				for(AbstractSubscriber subscriber : listOfSubscribers) {
@@ -54,7 +104,7 @@ public class Orchestration {
 			for(AbstractPublisher publisher : listOfPublishers) {
 				publisher.publish();
 			}
-			
+
 		} catch (IOException ioe) {
 			System.out.println(ioe.getMessage());
 			System.out.println("Will now terminate");
@@ -63,11 +113,11 @@ public class Orchestration {
 		for(AbstractPublisher publisher : listOfPublishers) {
 			publisher.publish();
 		}
-		
-		
+
+
 	}
 
-	
+
 	private List<AbstractPublisher> createPublishers() throws IOException{
 		List<AbstractPublisher> listOfPublishers = new ArrayList<>();
 		AbstractPublisher newPub;
@@ -86,7 +136,7 @@ public class Orchestration {
 		StrategyBufferedReader.close();
 		return listOfPublishers;
 	}
-	
+
 	private List<AbstractSubscriber> createSubscribers() throws IOException{
 		List<AbstractSubscriber> listOfSubscribers = new ArrayList<>();
 		AbstractSubscriber newSub;
@@ -105,7 +155,7 @@ public class Orchestration {
 		StateBufferedReader.close();
 		return listOfSubscribers;
 	}
-	
-	
-	
+
+
+
 }
